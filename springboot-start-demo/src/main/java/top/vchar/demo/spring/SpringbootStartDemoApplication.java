1c3d2651fafa73e31cbd836592bbcadd44ba1e4a
vchar_fred <vchar.fred@foxmail.com>
2019-08-04 12:14:13 +0800

    启动类

---
+++ springboot-start-demo/src/main/java/top/vchar/demo/spring/SpringbootStartDemoApplication.java
@@ -0,0 +1,19 @@
+package top.vchar.demo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> 启动类 </p>
+ *
+ * @author vchar fred
+ * @version 1.0
+ * @create_date 2019/8/4 11:45
+ */
+@SpringBootApplication
+public class SpringbootStartDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStartDemoApplication.class);
+    }
+}
