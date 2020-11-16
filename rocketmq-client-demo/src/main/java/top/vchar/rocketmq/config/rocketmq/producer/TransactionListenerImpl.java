d0917521d40b91b67783171cdbfcf3655fabef69
vchar_fred <2515513925@qq.com>
2020-11-16 16:32:26 +0800

    事务消息示例

--- rocketmq-client-demo/src/main/java/top/vchar/rocketmq/config/rocketmq/producer/TransactionListenerImpl.java
+++ rocketmq-client-demo/src/main/java/top/vchar/rocketmq/config/rocketmq/producer/TransactionListenerImpl.java
@@ -1,5 +1,6 @@
 package top.vchar.rocketmq.config.rocketmq.producer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.LocalTransactionState;
 import org.apache.rocketmq.client.producer.TransactionListener;
 import org.apache.rocketmq.common.message.Message;
@@ -7,7 +8,6 @@ import org.apache.rocketmq.common.message.MessageExt;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * <p> 事务消息监听器实现 </p>
@@ -16,29 +16,46 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @version 1.0
  * @create_date 2020/10/30
  */
+@Slf4j
 @Component
 public class TransactionListenerImpl implements TransactionListener {
 
-    private final AtomicInteger transactionIndex = new AtomicInteger(0);
-
     private final ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
 
-
+    /**
+     * 在MQ将消息投递成功后会调用这个方法
+     * @param msg 消息内容
+     * @param arg 自定义的业务参数
+     * @return 返回消息状态
+     */
     @Override
     public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
-        int value = transactionIndex.getAndIncrement();
-        int status = value % 3;
-        localTrans.put(msg.getTransactionId(), status);
+        String transactionId = msg.getTransactionId();
+        log.info("执行本地事务:{}", transactionId);
+
+        // 这里进行业务处理
+        try{
+            Thread.sleep(1000*10);
+            localTrans.put(msg.getTransactionId(), 2);
+            log.info("本地业务执行完成:{}", transactionId);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return LocalTransactionState.COMMIT_MESSAGE;
     }
 
     /**
-     * 检查本地事务
+     * 检查本地事务，这里就是补充机制，比如 executeLocalTransaction执行异常的时候，这里可以作为补偿处理
      */
     @Override
     public LocalTransactionState checkLocalTransaction(MessageExt msg) {
-        Integer status = localTrans.get(msg.getTransactionId());
+        String transactionId = msg.getTransactionId();
+        log.info("事务检查: {}", transactionId);
+        Integer status = localTrans.get(transactionId);
+
         if (null != status) {
+            localTrans.put(transactionId, 1);
             switch (status) {
                 case 0:
                     return LocalTransactionState.UNKNOW;
@@ -50,6 +67,6 @@ public class TransactionListenerImpl implements TransactionListener {
                     break;
             }
         }
-        return LocalTransactionState.COMMIT_MESSAGE;
+        return LocalTransactionState.UNKNOW;
     }
 }
--- rocketmq-client-demo/src/main/java/top/vchar/rocketmq/controller/TransactionMessageController.java
+++ rocketmq-client-demo/src/main/java/top/vchar/rocketmq/controller/TransactionMessageController.java
@@ -1,5 +1,6 @@
 package top.vchar.rocketmq.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.TransactionSendResult;
 import org.apache.rocketmq.common.message.Message;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.vchar.rocketmq.config.rocketmq.RocketProducerBuilder;
 import top.vchar.rocketmq.dto.OrderNotify;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 /**
  * <p> 事务消息 </p>
@@ -34,10 +38,15 @@ public class TransactionMessageController {
     @PostMapping
     public TransactionSendResult sendTransactionMessage(@Validated @RequestBody OrderNotify orderNotify){
 
-        Message message = new Message();
+
         try {
-            TransactionSendResult result = producerBuilder.build().sendMessageInTransaction(message, null);
-            return result;
+            Message message = new Message();
+            message.setTopic("activity-notify");
+            message.setTags("train");
+            message.setKeys(UUID.randomUUID().toString());
+            message.setBody(JSONObject.toJSONString(orderNotify).getBytes(StandardCharsets.UTF_8));
+
+            return producerBuilder.build().sendMessageInTransaction(message, null);
         } catch (MQClientException e) {
             e.printStackTrace();
         }
