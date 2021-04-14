7723d6c1b5245d04a1153be2e0da7ccd7ed5a706
vchar_fred <2515513925@qq.com>
2021-04-14 13:53:31 +0800

    eureka 注册中心示例

---
+++ eureka-standalone/src/main/java/top/vcahr/eureka/EurekaStandaloneApplication.java
@@ -0,0 +1,21 @@
+package top.vcahr.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p> eureka单机版启动 </p>
+ *
+ * @author vchar fred
+ * @version 1.0
+ * @create_date 2021/4/14
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaStandaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaStandaloneApplication.class, args);
+    }
+}
