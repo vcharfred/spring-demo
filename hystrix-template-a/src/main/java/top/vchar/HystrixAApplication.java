5d99c61ef38c7718dc5cc36ad04f9dff1f595c76
vchar_fred <2515513925@qq.com>
2021-06-22 14:48:56 +0800

    hystrix监控示例

--- hystrix-template-a/src/main/java/top/vchar/HystrixAApplication.java
+++ hystrix-template-a/src/main/java/top/vchar/HystrixAApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @version 1.0
  * @create_date 2021/6/20
  */
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
