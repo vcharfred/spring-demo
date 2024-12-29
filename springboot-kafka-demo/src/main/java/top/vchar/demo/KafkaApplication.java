f78d86310cf0fd60464c5f41a0db03bb4bcd83f8
vcharfred <vcharfred@gmail.com>
2024-12-29 23:29:40 +0800

    feat: spring-book-kafka 示例

---
+++ springboot-kafka-demo/src/main/java/top/vchar/demo/KafkaApplication.java
@@ -0,0 +1,18 @@
+package top.vchar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> 启动类 </p>
+ *
+ * @author vchar fred
+ * @date 2024/12/25
+ */
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
